b4e966e60c9f94f8656154e6f9f3857d2f9e1f22
jiweiqi10 <jiweiqi10@163.com>
2020-06-12 15:00:27 -0400

    multiple intiial conditions

--- sandbox/diffeq_case1_5s4r.jl
+++ sandbox/diffeq_case1_5s4r.jl
@@ -1,4 +1,6 @@
 using DiffEqFlux, OrdinaryDiffEq, Flux, Optim, Plots
+using Flux: gradient
+using Flux.Optimise: update!
 
 function trueODEfunc(dydt, y, k, t)
     dydt[1] = -2 * k[1] * y[1]^2 - k[2] * y[1]
@@ -8,16 +10,26 @@ function trueODEfunc(dydt, y, k, t)
     dydt[5] = k[4] * y[2] * y[4]
 end
 
-u0 = Float64[1.0;1.0;0.0;0.0;0.0]
+n_exp = 10
+ns = 5
+nr = 4
+u0_list = rand(Float64, (n_exp, ns))
+u0_list[:, 3:ns] .= 0
+
 datasize = 20
 tspan = Float64[0.0, 20.0]
 tsteps = range(tspan[1], tspan[2], length = datasize)
-
 k = Float64[0.1, 0.2, 0.13, 0.3]
 alg = Rosenbrock23(autodiff = false)
 
-prob_trueode = ODEProblem(trueODEfunc, u0, tspan, k)
-ode_data = Array(solve(prob_trueode, alg, saveat = tsteps))
+ode_data_list = []
+
+for i in 1:n_exp
+    u0 = u0_list[i, :]
+    prob_trueode = ODEProblem(trueODEfunc, u0, tspan, k)
+    ode_data = Array(solve(prob_trueode, alg, saveat = tsteps))
+    push!(ode_data_list, ode_data)
+end
 
 lb = 1e-5
 ub = 10.0
@@ -28,17 +40,33 @@ dudt2 = FastChain((x, p)->log.(clamp.(x, lb, ub)),
 
 prob_neuralode = NeuralODE(dudt2, tspan, alg, saveat = tsteps)
 p = prob_neuralode.p
-# layer1: 5x4+4 = 24
-# layer2: 4x5+5 = 25
-p[1:20] .= clamp.(p[1:20], 0, 2.5)
-p[45:49] .= 0.0 * p[45:49]
 
-function predict_neuralode(p)
-    return clamp.(Array(prob_neuralode(u0, p)), -ub, ub)
+function clip_p(p)
+    # layer1: 5x4+4 = 24
+    # layer2: 4x5+5 = 25
+    p[1:20] .= clamp.(p[1:20], 0, 2.5)
+    p[45:49] .= 0
+
+    return p
 end
 
-function loss_neuralode(p)
-    pred = predict_neuralode(p)
+function predict_neuralode(u0, p)
+    pred = clamp.(Array(prob_neuralode(u0, p)), -ub, ub)
+    return pred
+end
+
+function loss_neuralode(p, i_exp)
+    u0 = u0_list[i_exp, :]
+    ode_data = ode_data_list[i_exp]
+    pred = predict_neuralode(u0, p)
+    loss = sum(abs2, ode_data .- pred)
+    return loss
+end
+
+function loss_pred_neuralode(p, i_exp)
+    u0 = u0_list[i_exp, :]
+    ode_data = ode_data_list[i_exp]
+    pred = predict_neuralode(u0, p)
     loss = sum(abs2, ode_data .- pred)
     return loss, pred
 end
@@ -58,55 +86,62 @@ end
 list_plots = []
 list_loss = []
 iter = 0
-p_now = p
-cb = function (p, l, pred; doplot = true)
-    global list_plots, iter, p_now
+cb = function (p, i_exp; doplot = true)
+    global list_plots, iter
 
-    p[1:20] .= clamp.(p[1:20], 0, 2.5)
-    p[45:49] .= 0.0 * p[45:49]
+    p = clip_p(p)
 
-    p_now = p
+    loss, pred = loss_pred_neuralode(p, i_exp)
 
     if iter == 0
         list_plots = []
     end
     iter += 1
 
-    push!(list_loss, l)
-
-    # plot current prediction against data
+    push!(list_loss, loss)
 
     list_plt = []
 
+    ode_data = ode_data_list[i_exp]
+
     for i in 1:5
-        plt = scatter(tsteps, ode_data[i,:], label = string("data",i))
+        plt = scatter(tsteps, ode_data[i,:], title = string(i), label = string("data",i))
         plot!(plt, tsteps, pred[i,:], label = string("pred",i))
         push!(list_plt, plt)
     end
 
     if iter < 2000
-        plt_loss = plot(list_loss, yscale = :log10, label="loss")
+        plt_loss = plot(list_loss, yscale = :log10, label="loss", title = string("exp", i_exp))
     else
-        plt_loss = plot(list_loss, xscale = :log10, yscale = :log10, label="loss")
+        plt_loss = plot(list_loss, xscale = :log10, yscale = :log10, label="loss", title = string("exp", i_exp))
     end
 
     push!(list_plt, plt_loss)
 
-    plt_all = plot(list_plt..., layout = (2,3), legend = true)
+    plt_all = plot(list_plt..., layout = (2,3), legend = false)
 
-    #push!(list_plots, plt)
-    if doplot & (iter % 100 == 0)
-        display(plt_all)
-        display(iter)
-        display(l)
+    #push!(list_plots, plt_all)
+    if iter % 100 == 0
+        println(string("\n", "iter = ", iter, " i_exp = ", i_exp, " loss = ", loss, "\n"))
         display_p(p)
+        display(plt_all)
     end
 
     return false
 end
 
-pstart = DiffEqFlux.sciml_train(loss_neuralode, p, ADAM(0.001), cb = cb, maxiters = 10000).minimizer
 
-pstart = DiffEqFlux.sciml_train(loss_neuralode, pstart, ADAM(0.001), cb = cb, maxiters = 200000).minimizer
+opt = ADAM(0.001)
 
-pmin = DiffEqFlux.sciml_train(loss_neuralode, p, cb = cb, Optim.KrylovTrustRegion(), maxiters = 1000)
+p = clip_p(p)
+
+for epoch in 1:100000
+    global p, iter
+    i_exp = rand(1:n_exp)
+    grad = gradient(loss_neuralode, p, i_exp)[1]
+    update!(opt, p, grad)
+    p = clip_p(p)
+    #println(string("\n", "iter ", epoch, " i_exp ", i_exp))
+    #display_p(p)
+    cb(p, i_exp)
+end
